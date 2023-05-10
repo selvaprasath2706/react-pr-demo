71ec0b6feb03210a57b76c2716c9de310932e2a2
Pooja Kannan <pooja.kannan@LT-MAC-232.local>
2023-05-10 15:08:55 +0530

    spelling mistake checked

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learningg React
+          Learning React
         </a>
       </header>
     </div>
